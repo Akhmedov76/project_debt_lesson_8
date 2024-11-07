a71da9501c3c61ae439b85f6b686c06da4f52931
Jasur <ahmedovj5016@gmail.com>
2024-11-07 19:58:55 +0500

    create usermodel

--- app_users/models.py
+++ app_users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class UserModel(AbstractBaseUser, PermissionsMixin):
+    phone_number = models.CharField(max_length=15)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.phone_number
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+
